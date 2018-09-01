44955b175dabe77361d46f288d1c3e4ea891593e
Nick Moore <nick@zoic.org>
2018-09-01 23:25:57 +1000

    pwm for led channels

--- ports/fupy/litex_leds.c
+++ ports/fupy/litex_leds.c
@@ -19,10 +19,23 @@ const mp_obj_type_t litex_led_type;
 
 typedef struct _litex_led_obj_t {
     mp_obj_base_t base;
-    int num;
+    unsigned int addr;
 } litex_led_obj_t;
 
-STATIC litex_led_obj_t litex_leds[CAS_LEDS_COUNT] = {0};
+STATIC litex_led_obj_t litex_leds[CAS_LEDS_COUNT] = {
+#ifdef CSR_CAS_LED0_DUTY_ADDR
+       { { &litex_led_type }, CSR_CAS_LED0_DUTY_ADDR },
+#endif
+#ifdef CSR_CAS_LED1_DUTY_ADDR
+       { { &litex_led_type }, CSR_CAS_LED1_DUTY_ADDR },
+#endif
+#ifdef CSR_CAS_LED2_DUTY_ADDR
+       { { &litex_led_type }, CSR_CAS_LED2_DUTY_ADDR },
+#endif
+#ifdef CSR_CAS_LED3_DUTY_ADDR
+       { { &litex_led_type }, CSR_CAS_LED3_DUTY_ADDR },
+#endif
+       };
 
 STATIC mp_obj_t litex_led_make_new(const mp_obj_type_t *type_in,
 		size_t n_args, size_t n_kw, const mp_obj_t *args) {
@@ -32,8 +45,6 @@ STATIC mp_obj_t litex_led_make_new(const mp_obj_type_t *type_in,
 
 	switch (led_num) {
 	case 1 ... CAS_LEDS_COUNT:
-		litex_leds[led_num - 1].base.type = &litex_led_type;
-		litex_leds[led_num - 1].num = led_num;
 		return &litex_leds[led_num - 1];
 	default:
 		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError,
@@ -43,12 +54,12 @@ STATIC mp_obj_t litex_led_make_new(const mp_obj_type_t *type_in,
 
 void litex_led_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
 	litex_led_obj_t *self = self_in;
-	mp_printf(print, "LED(%u)", self->num);
+	mp_printf(print, "LED(%p)", self->addr);
 }
 
 STATIC mp_obj_t litex_led_read(mp_obj_t self_in) {
 	litex_led_obj_t *led = self_in;
-	bool state = cas_leds_out_read() & (1 << (led->num - 1));
+	bool state = MMPTR(led->addr);
 
 	return mp_obj_new_bool(state);
 }
@@ -56,29 +67,31 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(litex_led_read_obj, litex_led_read);
 
 STATIC mp_obj_t litex_led_on(mp_obj_t self_in) {
 	litex_led_obj_t *led = self_in;
-	char value = cas_leds_out_read();
-
-	cas_leds_out_write(value | (1 << (led->num - 1)));
-
+	MMPTR(led->addr) = 255;
 	return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(litex_led_on_obj, litex_led_on);
 
 STATIC mp_obj_t litex_led_off(mp_obj_t self_in) {
 	litex_led_obj_t *led = self_in;
-	char value = cas_leds_out_read();
-
-	cas_leds_out_write(value & ~(1 << (led->num - 1)));
-
+	MMPTR(led->addr) = 0;
 	return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(litex_led_off_obj, litex_led_off);
+
+STATIC mp_obj_t litex_led_duty(mp_obj_t self_in, mp_obj_t duty_in) {
+	litex_led_obj_t *led = self_in;
+	MMPTR(led->addr) = mp_obj_get_int(duty_in);
+	return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(litex_led_duty_obj, litex_led_duty);
 
 
 STATIC const mp_map_elem_t litex_led_locals_dict_table[] = {
 	{ MP_OBJ_NEW_QSTR(MP_QSTR_read), (mp_obj_t)&litex_led_read_obj },
 	{ MP_OBJ_NEW_QSTR(MP_QSTR_on), (mp_obj_t)&litex_led_on_obj },
 	{ MP_OBJ_NEW_QSTR(MP_QSTR_off), (mp_obj_t)&litex_led_off_obj },
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_duty), (mp_obj_t)&litex_led_duty_obj },
 };
 STATIC MP_DEFINE_CONST_DICT(litex_led_locals_dict, litex_led_locals_dict_table);
 
